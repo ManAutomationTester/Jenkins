d60e9c2527066037e74244e4c2402d2c101d525d
Man Singh <man.singh@ostrumtech>
2024-04-28 12:29:35 +0100

    first commit

---
+++ src/test/java/jenkins/TestJenkins.java
@@ -0,0 +1,19 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+	
+	@Test
+	public void demoJenkinsMethod1() {
+		System.out.println("demo Jenkins Method 1");
+		
+	}
+	
+	@Test
+	public void demoJenkinsMethod2() {
+		System.out.println("demo Jenkins Method 2");
+//		int i=7/0;
+	}
+
+}
