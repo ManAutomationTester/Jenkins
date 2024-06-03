e38f044d042ab504d10a230fcc020c5157bcc7d7
Man Singh <man.singh@ostrumtech>
2024-06-03 19:03:21 +0100

    get extent in desktop

--- src/test/java/jenkins/TestJenkins.java
+++ src/test/java/jenkins/TestJenkins.java
@@ -1,5 +1,10 @@
 package jenkins;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TestJenkins {
@@ -10,10 +15,19 @@ public class TestJenkins {
 		
 	}
 	
-	@Test
+	@AfterMethod
 	public void demoJenkinsMethod2() {
-		System.out.println("demo Jenkins Method 2");
-//		int i=7/0;
+		System.out.println("final report");
+		Desktop desk=   Desktop.getDesktop();
+      try {
+		desk.open(new File("D:\\OstrumTech\\ostrumTech_WorkSpace\\jenkins\\test-output\\emailable-report.html"));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
+ 
+	}
+	
+
 
 }
